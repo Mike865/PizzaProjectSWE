ba85b8c40ced22666c7f7874627f8a89a245abac
Vincent <vincentcradler@gmail.com>
2022-04-13 13:40:51 -0700

    made a save option

--- PizzaProjectSWE/Program.cs
+++ PizzaProjectSWE/Program.cs
@@ -9,7 +9,17 @@ namespace PizzaProjectSWE
     //Hello from comment
     static class Program
     {
-        /// <summary>
+        /*
+         * 4 Distinct pizzas
+         * 8 distinct toppings with combinations of up to 4 items
+         * 3 crust options
+         * 5 beverages
+         * 3 distinct beverage sizes
+         * Displaying the pricing of various itmes
+         * A display screen for the completed order showing items ordered the prices and totals
+         */
+        // Saving file
+         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
